74b090bf8b692801b1879118bf10fe96aa1d5b2f
ahmet keser <ahmetkeser006@gmail.com>
2023-10-13 22:57:52 +0300

    silice(dilimle)

--- w3.js
+++ w3.js
@@ -1,20 +1,51 @@
-let strElements = "Apple, Banana, Kiwi";
+//METİN PARÇALAMA İŞLEMLERİ 
+
+            // slice(start, end)
+            // substring(start, end)
+            // substr(start, length)
+
+
 document.getElementById('hstart').innerHTML="silice Dilimleme çalışması";
 
 
-
-function innerElement(sliceId,sliceAdd){            // id si ve içeğini alarak sayfaya ekleme yapar
+// id si ve içeğini alarak sayfaya ekleme yapar
+function innerElement(sliceId,sliceAdd){            
 document.getElementById(sliceId).innerHTML=sliceAdd;
 }
 let yazi
 innerElement("pstart",yazi)
+//********************************************************** */
 
-function addPBlock(text){           // gelen text tanımlı yazıyı ulStart id li li tagına p elementi oluşturup yazıyı kaydeder
+// gelen text tanımlı yazıyı ulStart id li li tagına p elementi oluşturup yazıyı kaydeder
+function addPBlock(text){
     let carryAdd= document.createElement("p")
     carryAdd.textContent=text
-    let comeCarry =document.getElementById('ulStart')
+    let comeCarry =document.getElementById('liStart')
     comeCarry.appendChild(carryAdd)
-    
 }
-yazi ="asdasdsad"
 addPBlock(yazi)
+//*********************************************************** */
+// slice(start, end) 0 dan başla end dahil değildir 
+let strElements = "Apple, Banana, Kiwi";
+addPBlock("silice(5,10) a dahil başla b dahil değil dizi mantığı 0 dan başla")
+addPBlock("Apple, Banana, Kiwi")
+addPBlock(strElements.slice(5,10))
+addPBlock("---------------------------------------------------------------------")
+addPBlock("silice(10) 10 dahil başla sonuna kadar yaz")
+addPBlock("Apple, Banana, Kiwi")
+addPBlock(strElements.slice(10))
+addPBlock("---------------------------------------------------------------------")
+addPBlock("silice(0,5) 0 dahil başla 5 (dahil değil) kadar yaz")
+addPBlock("Apple, Banana, Kiwi")
+addPBlock(strElements.slice(0,5))
+addPBlock("---------------------------------------------------------------------")
+addPBlock("silice(-12) dizinin sonundan saymaya başlar -12.eleman dahil değil")
+addPBlock("Apple, Banana, Kiwi")
+addPBlock(strElements.slice(-12))
+addPBlock("---------------------------------------------------------------------")
+addPBlock("silice(-12,-6) -12.eleman dahil değil, -6.eleman dahil")
+addPBlock("Apple, Banana, Kiwi")
+addPBlock(strElements.slice(-12,-6))
+
+//*********************************************************** */
+// substring(start, end)
