fc5311a059d5a932030e44fa7225a4f267df65c1
ahmet keser <ahmetkeser006@gmail.com>
2023-10-15 23:21:11 +0300

    pratatip çalışması

---
+++ protatip-yuzde.js
@@ -0,0 +1,60 @@
+                        // let cizgi = {
+                        //     altCizgi: "_",
+                        //     Walk(){
+                        //         //buraya fonksiyon gömebiliriz.
+                        //     }
+                        // };
+                        // let domain = {
+                        //     mail: "@gmail"
+                            
+                        // };
+                        // domain.__proto__ = cizgi; // obje içersinde kalıtımı __proto__: cizgi şeklinde de verebiliriz.
+                        // let firstName = "Ahmet"
+                        // let lastName = "KESER"
+
+                        // console.log(firstName+domain.altCizgi+lastName+domain.mail);
+
+                        // let a ={x:100,y:200};
+                        // let b =Object.create(a);
+                        // b.p=300;
+                        // b.q=400;
+                        // console.log(b.x,b.y,b.p,b.q);
+let carry1=document.createElement(InputEvent)
+let carry
+let l1Ogrenci={
+    __proto__ :genel,
+    ders1: "sosyal bilimlere giriş",
+    ders2: "oryantasyon"
+}
+let l2Ogrenci={
+    ders1:"sosyoloji",
+    ders2: "etkin beceri"
+}
+let l3Ogrenci={
+    ders1: "ileri sosyoloji",
+    ders2: "profosyonel beceri"
+}
+
+let genel ={
+    genel1: "tütkçe",
+    genel2: "matematik",
+    genel3 : "tarih",
+    genel4 : "sosyal bilimler"
+}
+let sayısal ={
+    __proto__ :genel,
+    sayısal1: "geometri",
+    sayısal2: "trigonometri",
+    sayısal3: "üssel kavramlar"
+}
+let sözel ={
+    __proto__:genel,
+    ders1: "ülkeler tarihi",
+    ders2: "derin tarih"
+}
+let oğrenci;
+if(ögrenci==1){
+    let ogrenciNo = Object.create(genel)
+    ogrenciNo=Object.create(l1Ogrenci)
+    if()
+}
