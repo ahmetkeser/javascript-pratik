5230e862efb6325dab6468d9f7bc952a211bce86
ahmet keser <ahmetkeser006@gmail.com>
2023-10-19 00:09:55 +0300

    yeni bir yöntem

---
+++ yeni.js
@@ -0,0 +1,11 @@
+debugger;// F10 ile adım adım takip edilebiliyor
+let control = confirm("seçiminiz");
+if (control==true){
+    console.log("devamm")
+}else{
+    console.log("devam etmek istemedi tekrarsor") 
+}
+debugger;
+location.reload(); // programı tekrar başlatır :D
+
+
