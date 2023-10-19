1412dbe4a1dbac9b96f8454e26a83202c21d9306
ahmet keser <ahmetkeser006@gmail.com>
2023-10-20 02:18:50 +0300

    20-10

--- aranan-deneme.js
+++ aranan-deneme.js
@@ -126,3 +126,60 @@
 // }
 // console.log(numbers);
 /******************************************************* */
+
+let numbers=[],tut=0,say=0;
+function randNumber(){
+    let number = Math.floor(Math.random()*9)+1;
+    return number;
+}
+function arrayControl(caseNumber){
+    if(numbers.includes(caseNumber)== true)
+    {
+        console.log("pass");
+        return 10;
+    }else{
+        numbers.push(caseNumber);
+        console.log("add :",caseNumber);
+        return caseNumber;
+    }
+}
+while(numbers.length<8){
+    console.log("Döngü sayısı :",say++);
+    switch( tut=randNumber()){
+        //console.log("switce giren sayı",tut)
+        case 1:
+            arrayControl(1);
+            break;
+        case 2:
+            arrayControl(2);
+            break;
+        case 3:
+            arrayControl(3);
+            break;
+        case 4:
+            arrayControl(4);
+            break;
+        case 5:
+            arrayControl(5);
+            break;
+        case 6:
+            arrayControl(6);
+            break;
+        case 7:
+            arrayControl(7);
+            break;
+        case 8:
+            arrayControl(8);
+            break;
+        case 9:
+            arrayControl(9);
+            break;
+        case 0:
+            break;
+        default:
+            console.log("geçersiz değer")
+            break;
+
+    }
+}
+console.log(numbers)
